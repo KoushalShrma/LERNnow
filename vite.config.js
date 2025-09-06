ab7f8b037b118f4904770de68709cd13a97423f2
KOUSHAL SHARMA <139133724+KoushalShrma@users.noreply.github.com>
2025-09-06 10:54:06 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-@@ .. @@
- import { defineConfig } from 'vite'
- import react from '@vitejs/plugin-react'
- import path from 'path'
- 
- export default defineConfig({
-   plugins: [react()],
-+  esbuild: {
-+    loader: 'jsx',
-+    include: /src\/.*\.[jt]sx?$/,
-+    exclude: []
-+  },
-   resolve: {
-     alias: {
-       '@': path.resolve(__dirname, './src'),
-     },
-   },
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: []
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
